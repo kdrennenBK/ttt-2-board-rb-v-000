03928b9985fd123aa1c25bfdc7b303e8adb200b7
Kate Drennen <k8ted11@gmail.com>
2017-12-10 03:24:22 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,14 @@
 # Define the variable board below.
 board = "Tic Tac Toe"
-row = [" ", " ", " "]
+row1 = [" ", " ", " "]
 row[0]
 row[1]
 row[2]
+row2 = [" ", " ", " "]
+row2[0]
+row2[2]
+row2[3]
+row3 = [" ", " ", " "]
+row3[0]
+row3[1]
+row3[2]
